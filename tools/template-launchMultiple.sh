3597f8faf0ef72eedb05d34d385878f2ff4ab4f4
Inaki Fernandez <inaki.fernandez@loria.fr>
2016-08-26 17:48:03 +0200

    Fixed bug in multiple launch

--- tools/template-launchMultiple.sh
+++ tools/template-launchMultiple.sh
@@ -13,7 +13,7 @@ nbRob='50 100 200 300 400'
 taskSeq='1,-1 2,-1'
 ctrlSetup='1 2'
 
-listProp=`parallel --header : echo R{1}.T{2}.B{3} gInitialNumberOfRobots={f1} gTaskSeq={f2} gBrait=f{3} ::: f1 $nbRob ::: f2 $taskSeq ::: f3 $ctrlSetup`
+listProp=`parallel --header : echo R{1}.T{2}.B{3} gInitialNumberOfRobots={f1} gTaskSeq={f2} gBrait={f3} ::: f1 $nbRob ::: f2 $taskSeq ::: f3 $ctrlSetup`
 
 #echo "$listProp"
 
@@ -32,7 +32,7 @@ suffix=`echo $line | cut -f1 -d" " | sed -e 's/,-1//' |sed -e 's/,/T/g'`
 
 cp $tmplate $outbasename-$suffix.properties
 mkdir $outlogbasename/$suffix
-
+echo $line
 echo $line | perl -p -e  's/^.*? //' | sed -e 's/ /\n/g' >> $outbasename-$suffix.properties
 
     for (( j=1; j<=$nbRuns; j++))
@@ -43,7 +43,7 @@ echo $line | perl -p -e  's/^.*? //' | sed -e 's/ /\n/g' >> $outbasename-$suffix
 done <<< "$listProp"
 
 
-parallel -j8 -a $commandFile
+parallel --dry-run -j8 -a $commandFile
 
 
 
